8991805f20df1461c02d0aefaa64f77ab3caed60
Errong Leng <errong.leng@gmail.com>
2021-08-07 18:00:15 -0700

    use er-nodejs-pptx to load worship template

--- pages/api/ppt.ts
+++ pages/api/ppt.ts
@@ -1,12 +1,25 @@
-import { assertProxies } from "mobx/dist/internal";
-
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const PPTX = require('nodejs-pptx')
+const PPTX = require('er-nodejs-pptx')
 const fs = require('fs')
+
+function unlinkCallback(err) {
+  if (err) {
+    console.log(err)
+  }
+}
+
+function errEnd(res, err, tmpPath) {
+  fs.unlink(tmpPath, unlinkCallback)
+  res.end(err)
+}
+
+function finishEnd(res, tmpPath) {
+  fs.unlink(tmpPath, unlinkCallback)
+  res.end()
+}
 
 export default async function handler(req, res) {
   console.log(req.body)
-
   res.writeHead(200, {
     'Content-Type': 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
   })
@@ -21,21 +34,9 @@ export default async function handler(req, res) {
   });
   await pptx.save(tmpPPTX)
   var rs = fs.createReadStream(tmpPPTX)
-  rs.on('error', function(err) {
-    fs.unlinkSync(tmpPPTX)
-    res.end(err)
-  })
-  rs.on('finish', function() {
-    fs.unlinkSync(tmpPPTX)
-    res.end()
-  })
-  res.on('error', function(err) {
-    fs.unlinkSync(tmpPPTX)
-    res.end(err)
-  })
-  res.on('finish', function() {
-    fs.unlinkSync(tmpPPTX)
-    res.end()
-  })
+  rs.on('error', (err) => errEnd(res, err, tmpPPTX))
+  rs.on('finish', () => finishEnd(res, tmpPPTX))
+  res.on('error', (err) => errEnd(res, err, tmpPPTX))
+  res.on('finish', () => finishEnd(res, tmpPPTX))
   rs.pipe(res)
 }
