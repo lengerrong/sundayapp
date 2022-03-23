636d5e14b41f2efde84f28860d2efbaffed8bebe
Errong Leng <errong.leng@gmail.com>
2022-03-23 01:40:22 -0700

    add psalm 1, 2, 3

---
+++ songs/psalms/2/index.ts
@@ -0,0 +1,28 @@
+import t1 from './1.txt'
+import t2 from './2.txt'
+import t3 from './3.txt'
+import t4 from './4.txt'
+
+export default {
+  'index': '1',
+  'title': '神膏立的君王必审判列国',
+  'type': 0,
+  'verses': [
+    {
+      'index': '1',
+      'text': t1
+    },
+    {
+      'index': '2',
+      'text': t2
+    },
+    {
+      'index': '3',
+      'text': t3
+    },
+    {
+      'index': '4',
+      'text': t4
+    }
+  ]
+}
